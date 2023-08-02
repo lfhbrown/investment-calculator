d23d5965577202d69737d486586acc7858e33b15
6Weapons <89061083+lfhbrown@users.noreply.github.com>
2023-08-01 21:21:28 -0700

    Cleaned up some of the comments

--- src/App.js
+++ src/App.js
@@ -21,10 +21,10 @@ function App() {
   useEffect(
     () => {
       setTableData(calculate(formData));
-    }, //The above function is the first argument in the useEffect function. It's comparable to the line of code in a ternary following the &&. It will run if the second argument (dependency array) is met.
+    }, //The above function is the first argument in the useEffect hook. It's comparable to the line of code in a ternary following the &&. It will run if the second argument (dependency array) is met.
     [formData]
   );
-  //the array above is called a dependency array. It's the second argument in the useEffect function that determines when the first argument is executed. In this case useEffect  is waiting for formData to change.
+  //the array above is called a dependency array. It's the second argument in the useEffect function that determines when the first argument is executed. In this case, useEffect is waiting for formData to change.
   //Once it does it will then call the setTableData function which we initialize with our helper function calculate and pass in the updated formData which we can pass to our FormTable component to display.
   console.log(formData);
 
